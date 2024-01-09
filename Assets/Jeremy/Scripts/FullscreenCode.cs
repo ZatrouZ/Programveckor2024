164cadc7403a80f5bfa9f908e9f0704eb4743713
JSG <jeremygustavson1@gmail.com>
2024-01-09 16:07:40 +0100

    M

--- Assets/Jeremy/Scripts/FullscreenCode.cs
+++ Assets/Jeremy/Scripts/FullscreenCode.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+
+//Denna kod gör så att spelet kan switcha emellan fullscreen och icke fullscreen.
 public class FullscreenCode : MonoBehaviour
 {
 

c65f555bea02793eada98d1017be4b26fb3149d8
Ricklus000 <albin.rissler@gmail.com>
2024-01-10 16:08:25 +0100

    cell1 klar

---
+++ Assets/Albin/scripts/key.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class key : MonoBehaviour
+{
+    pipe pipe;
+
+    [SerializeField]
+    GameObject keyText;
+    [SerializeField]
+    public GameObject keyInHand;
+    [SerializeField]
+    GameObject pipeOnGround;
+
+    bool reach;
+    public bool hasKey;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pipe = FindObjectOfType<pipe>();
+        keyText.SetActive(false);
+        keyInHand.SetActive(false);
+        pipeOnGround.SetActive(false);
+        hasKey = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (reach==true)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                gameObject.SetActive(false);
+                pipe.pipeInHand.SetActive(false);
+                keyInHand.SetActive(true);
+                pipeOnGround.SetActive(true);
+                hasKey = true;
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && pipe.hasPipe==true)
+        {
+            keyText.SetActive(true);
+            reach = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            keyText.SetActive(false);
+            reach = false;
+        }
+    }
+}

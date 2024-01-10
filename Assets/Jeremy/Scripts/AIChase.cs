8419baa9f0fc4b07cb4e2ae6a2f17a82c18cf603
JSG <jeremygustavson1@gmail.com>
2024-01-10 23:12:31 +0100

    trying to work on AI chase and AI patrol shit
    ITS COMPLICAted

--- Assets/Jeremy/Scripts/AIChase.cs
+++ Assets/Jeremy/Scripts/AIChase.cs
@@ -9,6 +9,8 @@ public class AIChase : MonoBehaviour
     public GameObject player;
     public float speed;
 
+    
+
     private float distance;
     // Start is called before the first frame update
     void Start()
@@ -19,15 +21,26 @@ public class AIChase : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
+
+
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
         direction.Normalize();
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        if (distance < 8)
+        if (distance < 4)
         {
+            GetComponent<EnemyPatrol>().enabled = false;
+            //GetComponent<AIChase>().enabled = true;
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-            
+    
+        }
+        else if(distance >5)
+        {
+            //GetComponent<EnemyPatrol>().enabled = true;
+            //GetComponent<AIChase>().enabled = false;
         }
         
 

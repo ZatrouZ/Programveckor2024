9c36201d9f584a3ae49c7462f0bad5341ddadf60
Ricklus000 <albin.rissler@gmail.com>
2024-01-11 14:59:53 +0100

    dokument rum färdig

--- Assets/Albin/scripts/dokument rum/desk.cs
+++ Assets/Albin/scripts/dokument rum/desk.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class desk : MonoBehaviour
 {
+    [SerializeField]
+    GameObject dokument;
+
+    bool deskSearch;
     // Start is called before the first frame update
     void Start()
     {
-        
+        dokument.SetActive(false);
+        deskSearch = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (deskSearch==true)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                dokument.SetActive(true);
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            deskSearch = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            dokument.SetActive(false);
+            deskSearch = false;
+        }
     }
 }

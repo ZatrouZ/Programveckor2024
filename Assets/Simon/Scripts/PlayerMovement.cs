2293b5c52725ebfbfdd61916857a45a65737c48d
Ricklus000 <albin.rissler@gmail.com>
2024-01-17 14:54:27 +0100

    movement fixat

--- Assets/Simon/Scripts/PlayerMovement.cs
+++ Assets/Simon/Scripts/PlayerMovement.cs
@@ -10,7 +10,7 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D rb;
 
     public Animator animator;
-    float L1R2U3D4;
+    float L1R2U3D4 = 1;
 
     [SerializeField]
     GameObject PauseMenuObject;
@@ -34,27 +34,46 @@ public class PlayerMovement : MonoBehaviour
             {
                 rb.AddForce(new Vector3(0, speed));
               //  animator.Play("RosaWalkUp");
-                animator.SetBool("movingUp", Input.GetKey(KeyCode.W));
+                animator.SetBool("movingUp", true);
+                animator.SetFloat("L1R2U3D4", 1);
+            }
+            else
+            {
+                animator.SetBool("movingUp", false);
             }
             if (Input.GetKey(KeyCode.S))
             {
                 rb.AddForce(new Vector3(0, -speed));
                  //animator.Play("RosaWalkDown");
-               animator.SetBool("movingDown", Input.GetKey(KeyCode.S));
+               animator.SetBool("movingDown", true);
+                animator.SetFloat("L1R2U3D4", 3);
+            }
+            else
+            {
+                animator.SetBool("movingDown", false);
             }
             if (Input.GetKey(KeyCode.A))
             {
                 rb.AddForce(new Vector3(-speed, 0));
                // animator.Play("RosaWalkLeft");
-                animator.SetBool("movingLeft", Input.GetKey(KeyCode.A));
+                animator.SetBool("movingLeft", true);
+                animator.SetFloat("L1R2U3D4", 5);
+            }
+            else
+            {
+                animator.SetBool("movingLeft", false);
             }
             if (Input.GetKey(KeyCode.D))
             {
                 rb.AddForce(new Vector3(speed, 0));
                // animator.Play("RosaWalkRight");
-                animator.SetBool("movingRight", Input.GetKey(KeyCode.D));
+                animator.SetBool("movingRight", true);
+                animator.SetFloat("L1R2U3D4", 7);
             }
-            
+            else
+            {
+                animator.SetBool("movingRight", false);
+            }
         }
         if (Input.GetKeyDown(KeyCode.Escape))
         {

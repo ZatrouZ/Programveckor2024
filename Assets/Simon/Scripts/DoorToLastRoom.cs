8567b0b814ce1681fe4095dfad4eb7b7b9ed2d6c
YodaOnKetamin <Simon.Ulander.06@gmail.com>
2024-01-18 15:57:53 +0100

    f

---
+++ Assets/Simon/Scripts/DoorToLastRoom.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class DoorToLastRoom : MonoBehaviour
+{
+    bool DoorOpen;
+    bool open;
+    public bool ConnectedOpen;
+
+    PhotonView View;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        View = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ConnectedOpen == true)
+        {
+            View.RPC("OpenLastDoor", RpcTarget.All);
+        }
+
+        if (open == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                SceneManager.LoadScene("sista rum(albin)");
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && DoorOpen == true)
+        {
+            open = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            open = false;
+        }
+    }
+
+    [PunRPC]
+    void OpenLastDoor() 
+    {
+        DoorOpen = true;
+    }
+}

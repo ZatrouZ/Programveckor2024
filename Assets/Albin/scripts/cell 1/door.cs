4836d57950dd1785fb67323b0cc2e74b5b89d47f
YodaOnKetamin <Simon.Ulander.06@gmail.com>
2024-01-19 11:25:26 +0100

    Update door.cs

--- Assets/Albin/scripts/cell 1/door.cs
+++ Assets/Albin/scripts/cell 1/door.cs
@@ -10,6 +10,7 @@ public class door : MonoBehaviour
 
     GameObject KeyInHand;
     PhotonView View;
+    GameObject Player1;
 
     bool open;
     // Start is called before the first frame update
@@ -17,18 +18,25 @@ public class door : MonoBehaviour
     {
         key = FindObjectOfType<key>();
         View = GetComponent<PhotonView>();
+        PhotonNetwork.AutomaticallySyncScene = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Player1 == null)
+        {
+            Player1 = GameObject.FindWithTag("Player");
+        }
+
         if (open == true)
         {
             KeyInHand = GameObject.FindWithTag("KeyInHand");
             if (Input.GetKey(KeyCode.E))
             {
                 View.RPC("RPC1", RpcTarget.All);
-                SceneManager.LoadScene("corridor 1(albin)");
+                //SceneManager.LoadScene("corridor 1(albin)");
+                PhotonNetwork.LoadLevel(5);
             }
         }
     }
@@ -52,6 +60,7 @@ public class door : MonoBehaviour
     void RPC1() 
     {
         KeyInHand.SetActive(false);
+        Player1.SetActive(false);
     }
 }
 

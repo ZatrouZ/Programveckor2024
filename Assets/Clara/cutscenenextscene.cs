c5a52a125225c2e4b6da09b0d45b10041f18108f
YodaOnKetamin <Simon.Ulander.06@gmail.com>
2024-01-19 11:52:17 +0100

    dfg

--- Assets/Clara/cutscenenextscene.cs
+++ Assets/Clara/cutscenenextscene.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Photon.Pun;
 
 public class cutscenenextscene : MonoBehaviour
 {
     private void OnEnable()
     {
-        SceneManager.LoadScene("StartingCells", LoadSceneMode.Single);
+        SceneManager.LoadScene("StartingCells");
     }
 }

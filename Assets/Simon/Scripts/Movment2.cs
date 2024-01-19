924cef3a18f1486ee8b3df5faebe83813e82d29a
YodaOnKetamin <Simon.Ulander.06@gmail.com>
2024-01-19 12:54:22 +0100

    mov

--- Assets/Simon/Scripts/Movment2.cs
+++ Assets/Simon/Scripts/Movment2.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class Movment2 : MonoBehaviour
 {
     [SerializeField]
     public float speed = 3;
     Rigidbody2D rb;
-
+    PhotonView View;
     public Animator animator;
 
     [SerializeField]
@@ -16,7 +17,7 @@ public class Movment2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        View = GetComponent<PhotonView>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -25,49 +26,52 @@ public class Movment2 : MonoBehaviour
     {
 
 
-        if (Input.GetKey(KeyCode.W))
+        if (View.IsMine)
         {
-            rb.AddForce(new Vector3(0, speed));
-            //  animator.Play("RosaWalkUp");
-            animator.SetBool("movingUp", true);
-            animator.SetFloat("L1R2U3D4", 1);
-        }
-        else
-        {
-            animator.SetBool("movingUp", false);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            rb.AddForce(new Vector3(0, -speed));
-            //animator.Play("RosaWalkDown");
-            animator.SetBool("movingDown", true);
-            animator.SetFloat("L1R2U3D4", 3);
-        }
-        else
-        {
-            animator.SetBool("movingDown", false);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            rb.AddForce(new Vector3(-speed, 0));
-            // animator.Play("RosaWalkLeft");
-            animator.SetBool("movingLeft", true);
-            animator.SetFloat("L1R2U3D4", 5);
-        }
-        else
-        {
-            animator.SetBool("movingLeft", false);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.AddForce(new Vector3(speed, 0));
-            // animator.Play("RosaWalkRight");
-            animator.SetBool("movingRight", true);
-            animator.SetFloat("L1R2U3D4", 7);
-        }
-        else
-        {
-            animator.SetBool("movingRight", false);
+            if (Input.GetKey(KeyCode.W))
+            {
+                rb.AddForce(new Vector3(0, speed));
+                //  animator.Play("RosaWalkUp");
+                animator.SetBool("movingUp", true);
+                animator.SetFloat("L1R2U3D4", 1);
+            }
+            else
+            {
+                animator.SetBool("movingUp", false);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                rb.AddForce(new Vector3(0, -speed));
+                //animator.Play("RosaWalkDown");
+                animator.SetBool("movingDown", true);
+                animator.SetFloat("L1R2U3D4", 3);
+            }
+            else
+            {
+                animator.SetBool("movingDown", false);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                rb.AddForce(new Vector3(-speed, 0));
+                // animator.Play("RosaWalkLeft");
+                animator.SetBool("movingLeft", true);
+                animator.SetFloat("L1R2U3D4", 5);
+            }
+            else
+            {
+                animator.SetBool("movingLeft", false);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                rb.AddForce(new Vector3(speed, 0));
+                // animator.Play("RosaWalkRight");
+                animator.SetBool("movingRight", true);
+                animator.SetFloat("L1R2U3D4", 7);
+            }
+            else
+            {
+                animator.SetBool("movingRight", false);
+            }
         }
 
 
--- Assets/Simon/Scripts/sINGELmOVMENT.cs
+++ Assets/Simon/Scripts/sINGELmOVMENT.cs
@@ -8,6 +8,7 @@ public class sINGELmOVMENT : MonoBehaviour
 [SerializeField]
     public float speed = 3;
     Rigidbody2D rb;
+    PhotonView View;
 
     public Animator animator;
 
@@ -17,19 +18,19 @@ public class sINGELmOVMENT : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        View = GetComponent<PhotonView>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-   
-        
+        if (View.IsMine)
+        {
             if (Input.GetKey(KeyCode.W))
             {
                 rb.AddForce(new Vector3(0, speed));
-              //  animator.Play("RosaWalkUp");
+                //  animator.Play("RosaWalkUp");
                 animator.SetBool("movingUp", true);
                 animator.SetFloat("L1R2U3D4", 1);
             }
@@ -40,8 +41,8 @@ public class sINGELmOVMENT : MonoBehaviour
             if (Input.GetKey(KeyCode.S))
             {
                 rb.AddForce(new Vector3(0, -speed));
-                 //animator.Play("RosaWalkDown");
-               animator.SetBool("movingDown", true);
+                //animator.Play("RosaWalkDown");
+                animator.SetBool("movingDown", true);
                 animator.SetFloat("L1R2U3D4", 3);
             }
             else
@@ -51,7 +52,7 @@ public class sINGELmOVMENT : MonoBehaviour
             if (Input.GetKey(KeyCode.A))
             {
                 rb.AddForce(new Vector3(-speed, 0));
-               // animator.Play("RosaWalkLeft");
+                // animator.Play("RosaWalkLeft");
                 animator.SetBool("movingLeft", true);
                 animator.SetFloat("L1R2U3D4", 5);
             }
@@ -62,7 +63,7 @@ public class sINGELmOVMENT : MonoBehaviour
             if (Input.GetKey(KeyCode.D))
             {
                 rb.AddForce(new Vector3(speed, 0));
-               // animator.Play("RosaWalkRight");
+                // animator.Play("RosaWalkRight");
                 animator.SetBool("movingRight", true);
                 animator.SetFloat("L1R2U3D4", 7);
             }
@@ -70,6 +71,9 @@ public class sINGELmOVMENT : MonoBehaviour
             {
                 animator.SetBool("movingRight", false);
             }
+        }
+        
+            
         
 
     }

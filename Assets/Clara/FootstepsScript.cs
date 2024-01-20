5182a906785cf21046d00a93b00d146b2d9d91de
lillamyy <114159749+lillamyy@users.noreply.github.com>
2024-01-20 12:14:33 +0100

    jag hatar unity

---
+++ Assets/Clara/FootstepsScript.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FootstepsScript : MonoBehaviour
+{
+  /*  public AudioSource audioSource;
+    public AudioClip[] audioClipArray;
+    AudioClip lastClip;
+
+    void Start()
+    {
+        audioSource.PlayOneShot(RandomClip());
+    }
+    
+    AudioClip RandomClip()
+    {
+        int attempt = 3;
+        AudioClip newClip = audioClipArray[Random.Range(0, audioClipArray.Length)];
+
+        while(newClip == lastClip && attempt > 0)
+        {
+           newClip = audioClipArray[Random.Range(0, audioClipArray.Length)];
+            attempt--;
+        }
+        lastClip = newClip;
+        return newClip;
+    }*/
+}

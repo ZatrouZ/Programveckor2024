d9649daffde0de3df13e29756dc625fbffbed793
lillamyy <114159749+lillamyy@users.noreply.github.com>
2024-01-21 12:44:44 +0100

    hmmmm

---
+++ Assets/Clara/chat.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class chat : MonoBehaviour
+{
+    /*[SerializeField] private GameObject chatUI = null;
+    [SerializeField] private TMP_Text chatText = null;
+    [SerializeField] private TMP_InputField inputField = null;
+
+    //need so that each player can chat 
+    private static event Action<string> OnMessage;
+
+    public override void OnStartAuthority()
+    {
+        chatUI.SetActive(true);
+
+        OnMessage -= HandleNewMessage; 
+    }
+
+    [ClientCallBack]
+    private void OnDestroy()
+    {
+        if (!hasAuthority) { return; }
+
+        OnMessage -= HandleNewMessage;
+    }
+
+    //what happens when message happend 
+    private void HandleNewMessage(string message)
+    {
+        chatText.text += message;
+    }
+
+    [Client]
+    public void send(string message)
+    {
+        if (!Input.GetKeyDown(KeyCode.Return)) { return; }
+        
+        if (string.IsNullOrWhiteSpace(message)) { return; }
+
+        CmdSendMessage(inputField.text);
+
+        //clear for next message
+        inputField.text = string.Empty;
+    }
+
+    [command]
+    private void CmdSendMessage(string message)
+    {
+        //same If we want to validate it here
+        RpcHandleMessage($"[{connectionToClient.connectionId}]: {message}");
+    }
+
+    [ClientRpc]
+    private void RpcHandleMessage(string message)
+    {
+        //same
+        OnMessage?.Invoke($"\n{message}");
+    }*/
+
+}

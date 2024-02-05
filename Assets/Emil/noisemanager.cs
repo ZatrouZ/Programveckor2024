fcbf3ac14a0aae783d6e1e36e842a7de582a3ec7
Kemilcurry <119925693+Kemilcurry@users.noreply.github.com>
2024-02-05 16:06:46 +0100

    h

--- Assets/Emil/noisemanager.cs
+++ Assets/Emil/noisemanager.cs
@@ -2,13 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class noisemanager : MonoBehaviour
+public class NoiseManager : MonoBehaviour
 {
     public float noise;
     public float totalNoise;
-    
-    private void Update()
-    { 
+    float timer;
 
+    private void Update()
+    {
+        totalNoise += Time.deltaTime / 5;
+        timer += Time.deltaTime;
+        if (timer >= 5)
+        {
+            print("fem");
+            timer = 0;
+            spawner();
+        }
+    }
+    void spawner()
+    {
+        print("spawned");
     }
 }

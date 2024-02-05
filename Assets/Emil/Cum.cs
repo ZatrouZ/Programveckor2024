acd685c795f37455ceb5c1e8a9e2f75c23827df5
Kemilcurry <119925693+Kemilcurry@users.noreply.github.com>
2024-02-05 16:26:14 +0100

    d

--- Assets/Emil/Cum.cs
+++ Assets/Emil/Cum.cs
@@ -32,7 +32,7 @@ public class Cum : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.GetComponent<>)
+        if (collision.GetComponent<PlayerMovement>())
         {
             hahaL();
         }

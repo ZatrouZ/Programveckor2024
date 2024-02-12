a1699dbca83e8ee19de913899cc352ee7ffd311e
YodaOnKetamin <Simon.Ulander.06@gmail.com>
2024-02-12 13:50:46 +0100

    Update BlackChat.cs

--- Assets/Simon/Scripts/BlackChat.cs
+++ Assets/Simon/Scripts/BlackChat.cs
@@ -4,18 +4,33 @@ using UnityEngine;
 
 public class BlackChat : MonoBehaviour
 {
+    bool yes = true;
+    bool no = true;
     GameObject chat;
     // Start is called before the first frame update
     void Start()
     {
-        chat = GameObject.FindWithTag("Chat");
-        StartCoroutine("blackchat");
+       
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (yes == true)
+        {
+            chat = GameObject.FindWithTag("Chat");
+        }
+        else if (no == true)
+        {
+            StartCoroutine("blackchat");
+            no = false;
+        }
+
+        if (chat != null)
+        {
+            yes = false;
+        }
     }
 
     IEnumerator blackchat() 

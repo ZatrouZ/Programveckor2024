44ca1ab38c4d2c2549f03e63970a1edb7b0e1213
JSG <jeremygustavson1@gmail.com>
2024-02-19 12:54:33 +0100

    wdwdw

--- Assets/Jeremy/Scripts/AIChase1.cs
+++ Assets/Jeremy/Scripts/AIChase1.cs
@@ -34,13 +34,15 @@ public class AIChase1 : MonoBehaviour
 
         if (distance < 4) //If distance is 8 or closer. This object where this script is placed will start following the player. However if further away than distance 9 it will stop following you.
         {
+            print("EnemyPatrolDisabled");
             GetComponent<EnemyPatrol>().enabled = false; //EnemyPatrol Disables
-            GetComponent<AIChase1>().enabled = true; //Ai chase script enables
+            //GetComponent<AIChase1>().enabled = true; //Ai chase script enables
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
 
         }
-        else if (distance >= 5)
+        else if (distance >= 6)
         {
+            print("EnemyPatrolEnabled");
             GetComponent<EnemyPatrol>().enabled = true; //EnemyPatrol Enables again if object is further away than distance 9.
 
 

421c515d2bb3c286783ca90cbb8e747bd6668026
JSG <jeremygustavson1@gmail.com>
2024-02-19 13:36:27 +0100

    AIChaseFixeduu8j

--- Assets/Albin/scripts/AIchase2.cs
+++ Assets/Albin/scripts/AIchase2.cs
@@ -9,13 +9,15 @@ public class AIchase2 : MonoBehaviour
 
     public float speed;
 
+    hidingplace hiding;
 
+    EnemyPatrol patrol;
 
     private float distance;
     // Start is called before the first frame update
     void Start()
     {
-
+        patrol = GetComponent<EnemyPatrol>();
     }
 
     // Update is called once per frame
@@ -30,18 +32,20 @@ public class AIchase2 : MonoBehaviour
         direction.Normalize();
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        if (distance < 4) //If distance is 8 or closer. This object where this script is placed will start following the player. However if further away than distance 9 it will stop following you.
+        if (hiding == false) //If distance is 8 or closer. This object where this script is placed will start following the player. However if further away than distance 9 it will stop following you.
         {
-            FindObjectOfType<EnemyPatrol>().enabled = false; //EnemyPatrol Disables
-            FindObjectOfType<AIChase>().enabled = true; //Ai chase script enables
-            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+
+            patrol.enabled = false; //EnemyPatrol Disables
+            //FindObjectOfType<AIChase>().enabled = true; //Ai chase script enables
+            print("moving");
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
 
         }
-        else if (distance >= 5)
-        {
-            FindObjectOfType<EnemyPatrol>().enabled = true; //EnemyPatrol Enables again if object is further away than distance 9.
-
-
+        
+        
+        else if (hiding == true)
+         {
+            FindObjectOfType<AIChase2>().enabled = false;
         }
 
 

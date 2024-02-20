1b9cf3b8a6a1103de3728c3d65ef4ee23ea6a7c6
YodaOnKetamin <Simon.Ulander.06@gmail.com>
2024-02-20 14:42:36 +0100

    dfgh

--- Assets/Simon/Scripts/Movemntscripts/sINGELmOVMENT.cs
+++ Assets/Simon/Scripts/Movemntscripts/sINGELmOVMENT.cs
@@ -39,49 +39,54 @@ public class sINGELmOVMENT : MonoBehaviour
         {
             NoiseManager = FindObjectOfType<NoiseManager>();
         }
-        if (Input.GetKey(KeyCode.W))
+
+        if (RealChat.isWriting == false)
         {
-            rb.AddForce(new Vector3(0, speed));
-            animator.SetBool("movingUp", true);
-            animator.SetFloat("L1R2U3D4", 1);
-            NoiseManager.totalNoise += Time.deltaTime / 5;
+            if (Input.GetKey(KeyCode.W))
+            {
+                rb.AddForce(new Vector3(0, speed));
+                animator.SetBool("movingUp", true);
+                animator.SetFloat("L1R2U3D4", 1);
+                NoiseManager.totalNoise += Time.deltaTime / 5;
+            }
+            else
+            {
+                animator.SetBool("movingUp", false);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                rb.AddForce(new Vector3(0, -speed));
+                animator.SetBool("movingDown", true);
+                animator.SetFloat("L1R2U3D4", 3);
+                NoiseManager.totalNoise += Time.deltaTime / 5;
+            }
+            else
+            {
+                animator.SetBool("movingDown", false);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                rb.AddForce(new Vector3(-speed, 0));
+                animator.SetBool("movingLeft", true);
+                animator.SetFloat("L1R2U3D4", 5);
+                NoiseManager.totalNoise += Time.deltaTime / 5;
+            }
+            else
+            {
+                animator.SetBool("movingLeft", false);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                rb.AddForce(new Vector3(speed, 0));
+                animator.SetBool("movingRight", true);
+                animator.SetFloat("L1R2U3D4", 7);
+                NoiseManager.totalNoise += Time.deltaTime / 5;
+            }
+            else
+            {
+                animator.SetBool("movingRight", false);
+            }
         }
-        else
-        {
-            animator.SetBool("movingUp", false);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            rb.AddForce(new Vector3(0, -speed));
-            animator.SetBool("movingDown", true);
-            animator.SetFloat("L1R2U3D4", 3);
-            NoiseManager.totalNoise += Time.deltaTime / 5;
-        }
-        else
-        {
-            animator.SetBool("movingDown", false);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            rb.AddForce(new Vector3(-speed, 0));
-            animator.SetBool("movingLeft", true);
-            animator.SetFloat("L1R2U3D4", 5);
-            NoiseManager.totalNoise += Time.deltaTime / 5;
-        }
-        else
-        {
-            animator.SetBool("movingLeft", false);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.AddForce(new Vector3(speed, 0));
-            animator.SetBool("movingRight", true);
-            animator.SetFloat("L1R2U3D4", 7);
-            NoiseManager.totalNoise += Time.deltaTime / 5;
-        }
-        else
-        {
-            animator.SetBool("movingRight", false);
-        }
+       
     }
 }

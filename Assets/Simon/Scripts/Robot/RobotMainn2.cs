5c7ad60f1ecd07da3b20fe988b1ebcca1a0ca690
YodaOnKetamin <Simon.Ulander.06@gmail.com>
2024-04-11 16:41:25 +0200

    er

--- Assets/Simon/Scripts/Robot/RobotMainn2.cs
+++ Assets/Simon/Scripts/Robot/RobotMainn2.cs
@@ -10,7 +10,7 @@ public class RobotMainn2 : MonoBehaviour
     public GameObject EndLocation;
     [SerializeField]
     GameObject Robot;
-    hidingplace2 hidingplace2;
+    HidingPlace2 hidingplace2;
     NoiseManager NoiseManager;
     public bool chasePlayer = true;
     GameObject HidingObj2;
@@ -44,7 +44,7 @@ public class RobotMainn2 : MonoBehaviour
         }
         else
         {
-            hidingplace2 = HidingObj2.GetComponent<hidingplace2>();
+            hidingplace2 = HidingObj2.GetComponent<HidingPlace2>();
         }
 
         if (hidingplace2 != null)
